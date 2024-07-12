0f4b9d6993142f0ad4b6c887e21b4a1b053a693a
Greg Pilkington <greg.j.pilkington@gmail.com>
2024-07-11 22:34:14 -0600

    Premature
    lot of moving parts currently, so gotta get done

--- app/components/new-test.js
+++ app/components/new-test.js
@@ -5,6 +5,16 @@ import { useState, useEffect } from "react";
 import CsvData from "@/app/components/csv-data";
 import NewPH from "@/app/components/new-ph";
 import NewTSS from "@/app/components/new-tss";
+import TestTypeSelect from "@/app/components/test-type";
+
+const CocRecord = {
+  "PH Conductivity": true,
+  "HPIC/IC": true,
+  Alkalinity: true,
+  TOCTIC: true,
+  ICP: true,
+  TSS: true,
+};
 
 const NewTest = () => {
   const [testType, setTestType] = useState("");
@@ -45,17 +55,6 @@ const NewTest = () => {
                   <div className="ml-4 font-bold">
                     <label>CoC Search:</label>
                   </div>
-                  {/* <div>
-                  <select
-                    className="border-2 rounded-lg p-3 bg-slate-400 w-52 h-12 text-center hover:bg-slate-500"
-                    id="dropdown"
-                    value={testType}
-                    onChange={(event) => setTestType(event.target.value)}
-                  >
-                    <option value=""> Type</option>
-                    <option value="PH Conductivity">PH Conductivity</option>
-                  </select>
-                </div> */}
                   <div className="ml-4 font-bold">
                     <label>Sample ID:</label>
                   </div>
@@ -83,27 +82,11 @@ const NewTest = () => {
             )}
           </div>
           <div className="test-input-pop mt-4 py-4 px-1">
-            <div className="test-navbar m-7">
-              <div className="mr-2 font-bold">
-                <label>Test Type:</label>
-              </div>
-              <div>
-                <select
-                  className="border-2 rounded-lg p-3 bg-slate-400 w-44 h-12 text-center hover:bg-slate-500"
-                  id="dropdown"
-                  value={testType}
-                  onChange={(event) => setTestType(event.target.value)}
-                >
-                  <option value="">Select a Test Type</option>
-                  <option value="PH Conductivity">PH Conductivity</option>
-                  <option value="HPIC/IC">HPIC/IC</option>
-                  <option value="Alkalinity">Alkalinity</option>
-                  <option value="TOCTIC">TOCTIC</option>
-                  <option value="ICP">ICP</option>
-                  <option value="TSS">TSS</option>
-                </select>
-              </div>
-            </div>
+            <TestTypeSelect
+              CocRecord={CocRecord}
+              testType={testType}
+              setTestType={setTestType}
+            />
           </div>
         </div>
       </div>
